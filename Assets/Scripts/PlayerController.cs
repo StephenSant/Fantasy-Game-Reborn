bcd9fc1d600a0a2e20ec17f0e2b80b258e60e4d5
Stephen <stephen.sant123@gmail.com>
2020-02-20 18:17:16 +1100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,18 +26,23 @@ public class PlayerController : MonoBehaviour
 
     public void Walk()
     {
+        //Gets input
         input = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
 
+        //Player faces forwards away from the camera
         transform.eulerAngles = Vector3.up * playerCam.transform.rotation.eulerAngles.y;
 
+        //uhh...
         Vector3 camF = playerCam.transform.forward;
         Vector3 camR = playerCam.transform.right;
 
+        //umm...
         camF.y = 0;
         camR.y = 0;
         camF = camF.normalized;
         camR = camR.normalized;
 
+        //I don't really know how this works
         transform.position += (camF*input.y + camR*input.x)*Time.deltaTime*walkSpeed;
     }
 }
